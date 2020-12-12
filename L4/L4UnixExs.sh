eeac07456f09506ab2deb08def903b1f6b65b1b6
edingtonk <edingtonkate@gmail.com>
2020-12-12 19:57:29 +0000

    Edited with answers

--- L4/L4UnixExs.sh
+++ L4/L4UnixExs.sh
@@ -2,24 +2,27 @@
 
 # -v suppresses header line
 echo -e "The number of people in the file:"
-grep -v -E  "^name|^\s" example_people_data.tsv | wc -l
+# NF is number of fields - assessing if there are 7 of them
+grep -v "name" example_people_data.tsv | awk ' {FS="\t"; if(NF == 7 && $1 != ""){print $0}}' | wc -l
 
 # how many people are aged around 30/older
 echo -e "The number of people aged around 30 or older"
-awk '($6 <= 1990)' example_people_data.tsv | wc -l
+# NEED TO THINK ABOUT BLANK LINES
+awk ' {FS="\t"; if($6 <= 1989 && NF == 7){print $0}}' example_people_data.tsv | wc -l
 
 # number of people called jan
 echo -e  "The number of people called Jan"
-awk '($1 == "Jan")' example_people_data.tsv | wc -l
+# remember to be specific!! need to think e.g. if i used ^ may include other names that start w/ jan
+awk '{FS="\t"; if($1 == "Jan"){print $0}}' example_people_data.tsv | wc -l
 
 # most common country of birth - from this country how many ppl are 50 or older
-# country is last field
-awk ' {if($7 == "Tokelau"){print $0}}' example_people_data.tsv > Tokelau.tsv
-# wc -l Tokelau.tsv
+# country is 7th field - cut -f7 outputs solely that field
+# sort sorts output alphabetically
+# uniq -c counts the no. nr countries and puts count next to country name
+# -k1,1 specifies sort file on first column (number) nr is number and reverse (descending) order
+cut -f7 example_people_data.tsv | sort | uniq -c | sort -k1,1nr | head -5
+echo -e "The number of people around 50 years old or less in the most common country in the file:"
+awk ' {FS="\t"; if($6 <= 1969 && $7 == "Mozambique"){print $0}}' example_people_data.tsv | wc -l
 
-awk ' {FS="\t"; if($7 == "United States"){print $0}}' example_people_data.tsv > UnitedStates.tsv
-echo -e  "The number of people who are around 50 or older:"
-awk '($6 <= 1970)' UnitedStates.tsv | wc -l
-
-# people with edu email
-awk ' {FS="\t"; if($2 == "edu$"){print $0}}' example_people_data.tsv > edu_email.tsv
+# people with edu email sorted by country then name in reverse alphabetical order 
+grep "\.edu" example_people_data.tsv | sort -k7,7 -k1,1r > edu.out
